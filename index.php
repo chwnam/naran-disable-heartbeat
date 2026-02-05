9038920644abf5319692e6df2edbd9ec475a1ee3
changwoo <ep6tri@hotmail.com>
2026-02-05 14:20:16 +0900

    Deregister script 'wp-auth-check' because it requires 'heartbeat' to be registered or, the core triggers 'incorrectly invoked WP_Scripts:add()' notice

--- index.php
+++ index.php
@@ -5,13 +5,22 @@
  * Author:      changwoo
  * Author URI:  mailto://ep6tri@hotmail.com
  * Plugin URI:  https://github.com/chwnam/naran-disable-heartbeat
- * Version:     1.0.0
+ * Version:     1.0.1
  * License:     GPLv2 or later
  */
 
-add_action( 'admin_init', 'ndh_deregister_heartbeat_script', 9999 );
-function ndh_deregister_heartbeat_script() {
-	if ( wp_script_is( 'heartbeat', 'registered' ) ) {
-		wp_deregister_script( 'heartbeat' );
-	}
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
+
+if ( ! function_exists( 'ndh_deregister_heartbeat_script' ) ) {
+    function ndh_deregister_heartbeat_script() {
+        if ( wp_script_is( 'heartbeat', 'registered' ) ) {
+            wp_deregister_script( 'heartbeat' );
+        }
+        if ( wp_script_is( 'wp-auth-check', 'registered' ) ) {
+            wp_deregister_script( 'wp-auth-check' );
+        }
+    }
+}
+add_action( 'admin_init', 'ndh_deregister_heartbeat_script', 9999 );
